5a6cfac448e4ad0c99c58800faed0dc4c87ca18d
mblack <mblack76@naver.com>
2022-11-18 18:52:48 +0900

    Config #1 - Spring configuration
    기초설정
    패키지에 빈이 있는지 확인한다.

---
+++ src/main/java/springpractice/config/Config.java
@@ -0,0 +1,9 @@
+package springpractice.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@ComponentScan("springpractice")
+@Configuration
+public class Config {
+}
